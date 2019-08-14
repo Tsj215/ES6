eee2c37a5547d46bb7133d78a908c7b81121501f
TSj215 <1035409298@qq.com>
2019-08-14 17:06:35 +0800

    函数扩展

--- ES6/day03/函数扩展.js
+++ ES6/day03/函数扩展.js
@@ -2,5 +2,17 @@ function test(a, b, c, d=1) {
 
 }
 
-console.log(test.name)
-console.log(test)
+// console.log(test.name)
+// console.log(test)
+
+function test() {
+    console.log(this)
+}
+
+let obj = {
+    name:'lil',
+    age:21,
+    test
+}
+
+obj.test()
