279c544d8ef8a8dcf1030a84b3fc45a62cb3ffff
TSj215 <1035409298@qq.com>
2019-08-14 17:22:27 +0800

    函数扩展

---
+++ ES6/day03/数组扩展.js
@@ -0,0 +1,11 @@
+//Array.from
+let temp = {'0':'hello','1':'world',length:2}
+console.log(Array.from(temp))
+console.log(Array.of(10))
+console.log(Array.of(10,11))
+
+let arr = [24,45,24,6,45,2]
+let set = new Set(arr)
+console.log(set)
+console.log(Array.from(set))
+console.log([...set])
